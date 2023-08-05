967cc882b609363b24d48bab5a44fcd1741d6d14
Lee <minmin1243@naver.com>
2023-08-05 17:48:09 +0900

    feat: Router 셋팅 완료

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ project/frontend/src/pages/Login.js
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>미니홈피</div>;
+};
+
+export default Login;
